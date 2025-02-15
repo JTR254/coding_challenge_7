d9722a2e5248e6f6003291cf684a0c36ebdb60b8
JTR254 <jackrindge@gmail.com>
2025-02-15 10:17:05 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -7,7 +7,7 @@ function calculateInvoice (subtotal, taxRate, discount) { // creates function th
 console.log (`Total Invoice: $${calculateInvoice (100, .08, 5).toFixed(2)}`) // logs the function to the console - Output: Total Invoice: $103.00
 console.log (`Total Invoice: $${calculateInvoice (500, .1, 20).toFixed(2)}`) // logs the function to the console - Output: Total Invoice: $530.00
 
-// Task 2 - Employee Hourly Wage 
+// Task 2 - Employee Hourly Wage Calculation
 
 let calculateHourlyWage = function wage (salary, hoursPerWeek) { // creates a function that calculates the hourly wage
     return salary / (hoursPerWeek * 52)
@@ -16,3 +16,17 @@ let calculateHourlyWage = function wage (salary, hoursPerWeek) { // creates a fu
 console.log (`Hourly Wage: $${calculateHourlyWage(52000, 40).toFixed(2)}`) // logs the function to the console - Output: Hourly Wage: $25.00
 console.log (`Hourly Wage: $${calculateHourlyWage(75000, 35).toFixed(2)}`) // logs the function to the console - Output: Hourly Wage: $41.21
 
+// Task 3 - Customer Loyalty Discount
+
+const calculateLoyaltyDiscount = (amount, years) => { // creates arrow function that determines discount based on time spent as a Loyalty Member
+    if (years >= 5) {
+        return amount * (1 - .15)
+    } else if (years >= 3) {
+        return amount * (1 - .1)
+    } else if (years < 3) {
+        return amount * (1 - .05)
+    }
+};
+
+console.log (`Discounted Price: $${calculateLoyaltyDiscount(100, 6).toFixed(2)}`) // logs the function to the console - Output: Discounted Price: $85.00
+console.log (`Discounted Price: $${calculateLoyaltyDiscount(200, 2).toFixed(2)}`) // logs the function to the console - Output: Discounted Price: $190.00
