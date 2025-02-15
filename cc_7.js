c665b6d6293fca895b73bc6ce08f5b175bd2e41f
JTR254 <jackrindge@gmail.com>
2025-02-15 09:48:33 -0500

    Task 2 - Employee Hourly Wage (also added .toFixed(2) to Task 1)

--- cc_7.js
+++ cc_7.js
@@ -4,8 +4,15 @@ function calculateInvoice (subtotal, taxRate, discount) { // creates function th
     return subtotal + (subtotal * taxRate) - discount
 };
 
-console.log (`Total Invoice: $${calculateInvoice (100, .08, 5)}`) // logs the function to the console - Output: Total Invoice: $103
-console.log (`Total Invoice: $${calculateInvoice (500, .1, 20)}`) // logs the function to the console - Output: Total Invoice: $530
+console.log (`Total Invoice: $${calculateInvoice (100, .08, 5).toFixed(2)}`) // logs the function to the console - Output: Total Invoice: $103.00
+console.log (`Total Invoice: $${calculateInvoice (500, .1, 20).toFixed(2)}`) // logs the function to the console - Output: Total Invoice: $530.00
 
+// Task 2 - Employee Hourly Wage 
 
+let calculateHourlyWage = function wage (salary, hoursPerWeek) { // creates a function that calculates the hourly wage
+    return salary / (hoursPerWeek * 52)
+};
+
+console.log (`Hourly Wage: $${calculateHourlyWage(52000, 40).toFixed(2)}`) // logs the function to the console - Output: Hourly Wage: $25.00
+console.log (`Hourly Wage: $${calculateHourlyWage(75000, 35).toFixed(2)}`) // logs the function to the console - Output: Hourly Wage: $41.21
 
